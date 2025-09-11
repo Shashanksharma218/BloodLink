a83f59db33f79625c55f88601d232a98f628f913
Shashank Sharma <babumanu2004@gmail.com>
2025-09-12 00:48:11 +0530

    "Add blood drop icon and update header component"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import DonorLoginModal from './DonorLoginModal'; // Import the modal
-
+import bloodIcon from "url:../assets/blood-drop.png";
 function Header() {
   const [isModalVisible, setModalVisible] = useState(false);
 
@@ -16,12 +16,13 @@ function Header() {
         <div className="container mx-auto px-6 md:px-20 py-4 flex justify-between items-center">
           {/* Logo and Title */}
           <Link to="/" className="flex items-center space-x-3">
-            <div className="w-10 h-10 bg-red-600 rounded-lg flex items-center justify-center">
+            {/* <div className="w-10 h-10 bg-red-600 rounded-lg flex items-center justify-center">
               <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
-            </div>
+            </div> */}
+            <img src={bloodIcon} className='h-11 w-11'></img>
             <div>
-              <div className="text-2xl font-semibold text-gray-900">BloodLink Una</div>
-              <div className="text-sm text-gray-500 -mt-1">Himachal Pradesh</div>
+              <div className="text-2xl font-semibold text-gray-900">BloodLink</div>
+              <div className="text-sm text-gray-500 -mt-1">Una, Himachal Pradesh</div>
             </div>
           </Link>
 
